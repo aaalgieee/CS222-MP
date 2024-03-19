3f911cf014b74c1be9be2cda4611d7bf8262be09
Al Gabriel <jaworski.orig@gmail.com>
2024-03-19 10:18:12 +0800

    Update MP1.asm

--- MP1.asm
+++ MP1.asm
@@ -1,85 +1,39 @@
+section .data
+    message db 'ORIG', 0
+
 section .text
     global _start
 
 _start:
-
-    ; Print 'O' character 
-    mov   eax, 4      ; set assembler to write
-    mov   ebx, 1      ; stdout fileno
-
-    push  'O'
-    mov   ecx, esp    ; esp now points to your char
-    mov   edx, 1      ; edx should contain how many characters to print
-    int   80h         ; sys_write(1, "O", 1)
-
+    ; Print 'O'
     mov eax, 4
     mov ebx, 1
-    push 10;
-    mov ecx, esp
-    mov edx,1 
-    int 80h
+    mov ecx, message
+    mov edx, 1
+    int 0x80
 
-    ; Print 'R' character 
-    mov   eax, 4      ; set assembler to write
-    mov   ebx, 1      ; stdout fileno
-
-    push  'R'
-    mov   ecx, esp    ; esp now points to your char
-    mov   edx, 1      ; edx should contain how many characters to print
-    int   80h         ; sys_write(1, "R", 1)
-
+    ; Print 'R'
     mov eax, 4
     mov ebx, 1
-    push 10;
-    mov ecx, esp
-    mov edx,1 
-    int 80h
+    mov ecx, message + 1
+    mov edx, 1
+    int 0x80
 
-    ; Print 'I' character 
-    mov   eax, 4      ; set assembler to write
-    mov   ebx, 1      ; stdout fileno
-
-    push  'I'
-    mov   ecx, esp    ; esp now points to your char
-    mov   edx, 1      ; edx should contain how many characters to print
-    int   80h         ; sys_write(1, "I", 1)
-
+    ; Print 'I'
     mov eax, 4
     mov ebx, 1
-    push 10;
-    mov ecx, esp
-    mov edx,1 
-    int 80h
+    mov ecx, message + 2
+    mov edx, 1
+    int 0x80
 
-    ; Print 'G' character 
-    mov   eax, 4      ; set assembler to write
-    mov   ebx, 1      ; stdout fileno
-
-    push  'G'
-    mov   ecx, esp    ; esp now points to your char
-    mov   edx, 1      ; edx should contain how many characters to print
-    int   80h         ; sys_write(1, "G", 1)
-
+    ; Print 'G'
     mov eax, 4
     mov ebx, 1
-    push 10;
-    mov ecx, esp
-    mov edx,1 
-    int 80h
+    mov ecx, message + 3
+    mov edx, 1
+    int 0x80
 
-
-    ; return value in EAX = 1 (byte written), or error (-errno)
-
-    add   esp, 4      ; restore esp if necessary
-
-    ; System exit
-    mov eax,1            
-    mov ebx,0            
-    int 80h
-
-
-   ;sudo apt-get update
-    ;sudo apt-get -y install nasm
-    ;nasm -f elf act1.asm
-    ;ld -m  elf_i386 act1.o -o act1
-    ;./act1
+    ; Exit the program
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
